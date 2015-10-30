65d2b1a450135d6cd4f1abf3754a086f633a7512
Paul <go2mailshan@gmail.com>
2015-10-31 03:24:32 +0530

    scaffolding

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # ng2-examples
-This repo is just to show different Angular 2.0 syntaxes with the help of examples
+This repo is just to show different Angular 2.0 syntaxes with the help of examples.
+
+
---
+++ app.ts
@@ -0,0 +1,11 @@
+import {Component,View, bootstrap} from 'angular2/angular2';
+@Component({
+    selector: 'my-app',
+})
+
+@View({
+    template: '<h1>Hello World</h1>'
+})
+
+class App { }
+bootstrap(App);
