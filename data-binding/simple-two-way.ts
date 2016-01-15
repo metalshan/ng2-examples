75929199e9162b202f2cdfee8ae5b72cf91ca560
Paul <go2mailshan@gmail.com>
2016-01-15 16:44:16 +0530

    little modification on two way binding example

--- data-binding/simple-two-way.ts
+++ data-binding/simple-two-way.ts
@@ -14,8 +14,14 @@ import {Component, View, FORM_DIRECTIVES} from 'angular2/angular2';
 	    <h2>Simple two way binding</h2>
 	    <input type="text" #my-input [(ng-model)]="myValue" />
 	    <p>Input value is : {{myValue}}</p>
+	    <button (click)="changeToRandom()">Change to random</button>
+
 	    `
 })
 export class TwoWayDataBinding {
 	myValue = 'Paul Shan';
+	changeToRandom(){
+		var aNum = Math.random();
+		this.myValue = aNum;
+	}
 }
