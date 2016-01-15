e21ef816368dc8b9b6ab82e20e99530d8ceec26c
Paul <go2mailshan@gmail.com>
2016-01-15 17:13:00 +0530

    both one and two way nested component binding

---
+++ data-binding/nested-component-binding-two-way/inner.ts
@@ -0,0 +1,25 @@
+/*
+	This is inner component of nested data binding example
+*/
+
+import {Component, View, FORM_DIRECTIVES, Input, Output, EventEmitter} from 'angular2/angular2';
+@Component({
+	selector: 'inner-component-db',
+
+})
+@View({
+	directives: [FORM_DIRECTIVES]
+	template: `
+	    <h3>Thhis is nested inner</h3>
+	    <input type="text" #my-input [value]="hisValue" (keyUp)="onHisValueChang(myInput)" />
+	    <p>Input value is : {{hisValue}}</p>
+	    `
+})
+export class InnerComponentDataBinding {
+	@Input() hisValue:any;
+	@Output() hisValueChange = new EventEmitter();
+	onHisValueChang(element){
+		this.hisValue = element.value;
+		this.hisValueChange.next(this.hisValue)
+	}
+}
---
+++ data-binding/nested-component-binding-two-way/outer.ts
@@ -0,0 +1,26 @@
+/*
+	This is outer component of nested data binding example
+*/
+
+import {Component, View, FORM_DIRECTIVES} from 'angular2/angular2';
+import {InnerComponentDataBinding} from './inner.ts';
+
+@Component({
+	selector: 'outer-component-db',
+
+})
+@View({
+	directives: [FORM_DIRECTIVES, InnerComponentDataBinding]
+	template: `
+	    <h3>This is nested outer</h3>
+	    <input type="text" #my-input [(ng-model)]="myValue" />
+	    <p>Input value is : {{myValue}}</p>
+	    <inner-component-db [(his-value)]="myValue"></inner-component-db>
+	    `
+})
+export class OuterComponentDataBinding {
+	myValue = "Paul Shan";
+	onHisValueChange(val){
+		this.myValue = val;
+	}
+}
